462c2c6e26627436080ed79496ea8116c385bd89
merunhowlader <merunhowlader@gmail.com>
2021-05-17 05:31:09 +0600

    hello world

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
-const fileUpload = require("express-fileUpload");
 require("dotenv").config();
-const fs =require("fs-extra");
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z94oz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -13,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('doctors'));
-app.use(fileUpload());
+
 
 const port = 5000;
 
